13574a822e058459e334358669d1159995a17b5a
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-09 19:04:34 +0530

    Swaping num without third variable

--- DSA Practice/BasicJava.java
+++ DSA Practice/BasicJava.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BasicJava {
     static void printIndividualLine(){
         
@@ -20,6 +21,16 @@ public class BasicJava {
         
     }
     public static void main(String args[]){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("Value after reversing");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
     }
 }
