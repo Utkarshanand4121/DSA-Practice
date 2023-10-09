9b9666776bf4d921f892f319079d9144b085c880
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-09 10:35:31 +0530

    Java Input Output

---
+++ DSA Practice/BasicJava.java
@@ -0,0 +1,25 @@
+public class BasicJava {
+    static void printIndividualLine(){
+        
+        // Your code here
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+
+    static void dataTypes(int a, float b, double c, long l, byte d){
+        
+        double p = c/b;//c/b
+        double q = b/a;//b/a
+        double r = c/a;//c/a
+        double m = r+l;//r+l
+        int s = a/d;//a/d
+        
+        //Printing all the results 
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+        
+    }
+    public static void main(String args[]){
+
+    }
+}
