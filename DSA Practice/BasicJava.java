2c8bda37cc3340e4cc514f5742b05ff66fc039c0
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-10 01:31:19 +0530

    comment

--- DSA Practice/BasicJava.java
+++ DSA Practice/BasicJava.java
@@ -20,6 +20,16 @@ public class BasicJava {
         System.out.println(p + " " + q + " " + r + " " + m + " " + s);
         
     }
+
+    static void comment(int a, int b, int c){
+        
+        //Prints a
+        System.out.println(a);
+        //Prints b
+        //System.out.println(b);
+        //Prints c
+        System.out.println(c);
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a: ");
