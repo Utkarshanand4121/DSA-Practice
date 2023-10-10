9dacc3011e4758afeeec86598afa819ad676a8e6
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-10 17:19:56 +0530

    college exam practice

--- DSA Practice/CollegePractice.java
+++ DSA Practice/CollegePractice.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class CollegePractice {
     public static int fact(int n) {
         int fact = 1;
@@ -21,7 +22,14 @@ public class CollegePractice {
             System.out.println(i+ " x " + i + " = "+i*i);
         }
     }
+    public static void greater(int a, int b){
+        if(a>b){
+            System.out.println(a+" is greater");
+        } else{
+            System.out.println(b+" is greater");
+        }
+    }
     public static void main(String args[]){
-        square(10);
+        greater(10, 15);
     }
 }
