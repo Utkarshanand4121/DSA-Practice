57556e90491a859f238ba73c66045f2555fd81a7
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-10 09:12:48 +0530

    String input

--- DSA Practice/BasicJava.java
+++ DSA Practice/BasicJava.java
@@ -30,6 +30,23 @@ public class BasicJava {
         //Prints c
         System.out.println(c);
     }
+    static void getInput () {
+		
+	    Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt(); //Taking the number of testcases
+	    while(t-->0)
+	    {
+    		int a = sc.nextInt(); 
+    		sc.nextLine();
+    	    String s = sc.nextLine();
+    	    
+    	    //Your code here
+    
+    		System.out.println(a);
+    		System.out.println(s);
+	    }
+	
+	}
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a: ");
