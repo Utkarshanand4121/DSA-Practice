52508a0ed3863e54a49170584b358095fa4a8337
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-19 20:29:34 +0530

    Array revision

--- Revision/ArrayRevision.java
+++ Revision/ArrayRevision.java
@@ -99,10 +99,43 @@ public class ArrayRevision {
 
     // Prefix method
     public static void prefix(int arr[]) {
-        
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            int start = i;
+            for(int j=i; j<arr.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+            }
+            if(maxSum < currSum) {
+                maxSum = currSum;
+            }
+        }
+        System.out.println("max sum is: " + maxSum);
+    }
+
+    public static void kadens(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i=0; i<arr.length; i++) {
+            currSum += arr[i];
+            if(currSum<0) {
+                currSum = 0;
+            }
+        }
+        if(maxSum < currSum) {
+            maxSum = currSum;
+        }
+        System.out.println("max sum is: " +maxSum);
     }
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5 };
-        maxSum(arr);
+        kadens(arr);
     }
 }
