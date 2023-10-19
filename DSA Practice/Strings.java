21f0351372637e2f3791c3f4156babcb012610cc
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-19 23:48:36 +0530

    String Revision

--- DSA Practice/Strings.java
+++ DSA Practice/Strings.java
@@ -55,6 +55,17 @@ public class Strings {
         }
         return greater;
     }
+
+    int isPalindrome(String S) {
+        // code here
+        int n = S.length();
+        for(int i=0; i<n/2; i++){
+            if(S.charAt(i) != S.charAt(n-i-1)){
+                return 0;
+            }
+        }
+        return 1;
+    }
     public static void main(String[] args) {
 
     }
