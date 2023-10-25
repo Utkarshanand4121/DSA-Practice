e68d4c230bedabf2dc666d513a3c2174bb7409f9
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-25 23:20:07 +0530

    Array Practice

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -322,6 +322,33 @@ public class Array {
         }
         return true;
     }
+
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        // code here
+        int a[] = new int[n];
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i<n; i++) {
+            if(arr[i] <= x) {
+                count1++;
+            }
+            if(arr[i] >= x) {
+                count2++;
+            }
+        }
+        a[0] = count1;
+        a[1] = count2;
+        return a;
+        
+    }
+
+    int thirdLargest(int a[], int n)
+    {
+	    // Your code here
+	    Arrays.sort(a);
+	    int result = 0;
+	    return a[n-3];
+    }
     public static void main(String[] args) {
 
     }
