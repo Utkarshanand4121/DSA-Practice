a0e4e2e93978deb6964b4c00d82fb90f09a94f03
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-26 17:59:04 +0530

    Most Time Taking Question

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -345,10 +345,24 @@ public class Array {
     int thirdLargest(int a[], int n)
     {
 	    // Your code here
+        
 	    Arrays.sort(a);
 	    int result = 0;
 	    return a[n-3];
     }
+
+    int Maximize(int arr[], int n)
+    {
+        // Complete the function
+        Arrays.sort(arr);
+        long sum = 0;
+        long mod = 1000000007;
+        for (int i = 0; i < n; i++) {
+            sum = (sum + (long) arr[i] * i) % mod;
+        }
+        return (int) sum;
+    }
+    
     public static void main(String[] args) {
 
     }
