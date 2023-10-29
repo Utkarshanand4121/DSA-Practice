016467e2965bba96a694d45f592995d80e655295
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-29 20:26:59 +0530

    String Practice

--- DSA Practice/Strings.java
+++ DSA Practice/Strings.java
@@ -123,6 +123,15 @@ public class Strings {
             return "No";
         }
     }
+
+    public int lastIndex( String s) {
+        for(int i=s.length()-1; i>=0; i--) {
+            if(s.charAt(i) == '1') {
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
 
     }
