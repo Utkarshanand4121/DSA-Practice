d0d4f63ad0fcb1ac147f0b854c545d77f5155f21
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-10-29 11:43:46 +0530

    Array Practice

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -430,6 +430,16 @@ public class Array {
         * No need to print the array
         * ************/
     }
+
+    public void rotate(int arr[], int n)
+    {
+        int x = arr[n-1];
+        for(int i=n-1; i>0; i--) {
+            arr[i] = arr[i-1];
+            
+        }
+        arr[0] = x;
+    }
     public static void main(String[] args) {
 
     }
