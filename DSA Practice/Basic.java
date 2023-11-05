02a1969538312ea8ef05b98f0b0799ff8b06fd7d
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-05 19:21:13 +0530

    Java Practice

--- DSA Practice/Basic.java
+++ DSA Practice/Basic.java
@@ -127,6 +127,29 @@ public class Basic {
         }
         return count;
     }
+    int middle(int A, int B, int C){
+        //code here
+        int num = Integer.MAX_VALUE;
+        if((A>B && A<C) || (A<B && A>C)) {
+            num = A;
+        } else if((B<A && B>C ) || (B>A && B<C)) {
+            num = B;
+        } else {
+            num = C;
+        }
+        return num;
+    }
+
+    static String isValid(Long N){
+        // code here
+        String str = "";
+        if(N%5==0) {
+            str = "YES";
+        } else {
+            str = "NO";
+        }
+        return str;
+    }
     public static void main(String args[]){
         // Scanner sc = new Scanner(System.in);
         // System.out.print("Enter a: ");
