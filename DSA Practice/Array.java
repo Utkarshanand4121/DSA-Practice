f70a7b66856d6e750b0255e0ceea256fc8af2cce
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-07 14:14:01 +0530

    Array Prctice

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -462,6 +462,19 @@ public class Array {
         Arrays.sort(arr);
         return arr;
     }
+
+    public static int multiply (int arr[], int n) {
+        //Complete the function
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i=0; i<n/2; i++) {
+            sum1 += arr[i]; 
+        }
+        for(int i=n-1; i>=n/2; i--) {
+            sum2 += arr[i];
+        }
+        return sum1*sum2;
+    }
     public static void main(String[] args) {
 
     }
