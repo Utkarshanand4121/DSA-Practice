0c6de2c70c59495c928ee47c0306de715d06223e
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-16 00:23:51 +0530

    Pattern Practice

--- DSA Practice/Pattern.java
+++ DSA Practice/Pattern.java
@@ -109,6 +109,26 @@ public class Pattern {
             System.out.println();
         }
     }
+
+    void printTrianglee(int n) {
+        // code here
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    void printTriangleee(int n) {
+        // code here
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         printTriangle5(5);
     }
