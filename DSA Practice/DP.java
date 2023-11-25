6abb601a1ca751abc90bea364f88cf4a57ee1077
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-11-25 07:27:10 +0530

    .

--- DSA Practice/DP.java
+++ DSA Practice/DP.java
@@ -11,6 +11,19 @@ public class DP {
         return dp;
         
     }
+
+    static long fibSum(long N){
+        //code here
+        long dp[] = new long[(int)N+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        long sum = 1;
+        for(int i=2; i<=N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+            sum = (sum + dp[i])%1000000007;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         
     }
