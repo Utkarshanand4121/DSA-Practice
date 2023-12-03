54a16e5d24f74344eef4d0945b7ac9b63c644dbc
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-03 06:53:47 +0530

    Practice

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -667,6 +667,29 @@ public class Array {
             }
         }
     }
+
+    int[]  select(int arr[], int i)
+	{
+        // code here such that selectionSort() sorts arr[]
+        selectionSort(arr, i);
+        return arr;
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    for(int i=0; i<n-1; i++) {
+	        int minpos = i;
+	        for(int j=i+1; j<n; j++) {
+	            if(arr[minpos] > arr[j]) {
+	                minpos = j;
+	            }
+	        }
+	        int temp = arr[minpos];
+	        arr[minpos] = arr[i];
+	        arr[i] = temp;
+	    }
+	}
     public static void main(String[] args) {
 
     }
