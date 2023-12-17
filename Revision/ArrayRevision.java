db3dbc8f31525056c7508d792bfab519725144d7
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-17 17:46:38 +0530

    Array Revision

--- Revision/ArrayRevision.java
+++ Revision/ArrayRevision.java
@@ -139,27 +139,42 @@ public class ArrayRevision {
         // calculating left max boundary
         int leftMax[] = new int[height.length];
         leftMax[0] = height[0];
-        for(int i=1; i<height.length; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
         // calculating right max boundary
         int rightMax[] = new int[height.length];
-        rightMax[height.length-1] = height[height.length-1];
-        for(int i=height.length-2; i>=0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
         // calculating trapped water
         int trappedWater = 0;
-        for(int i=0; i<height.length; i++) {
+        for (int i = 0; i < height.length; i++) {
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
             trappedWater += waterLevel - height[i];
         }
         return trappedWater;
     }
 
+    public static int buyAndSell(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
-        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
-        System.out.println(trapped(height));
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndSell(prices));
     }
 }
