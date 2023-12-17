a6b01762345ba38f994d06bbbd56862d2901f389
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-17 20:07:38 +0530

    Revision on full mode

--- Revision/ArrayRevision.java
+++ Revision/ArrayRevision.java
@@ -173,8 +173,28 @@ public class ArrayRevision {
         return maxProfit;
     }
 
+    // Search in sorted matrix
+    public static boolean search(int arr[][], int key) {
+        int row = 0;
+        int col = arr[0].length-1;
+        while(row < arr.length && col > 0) {
+            if(key == arr[row][col]) {
+                System.out.println("Key found at ("+row+"," +col+")");
+                return true;
+            } else if(key < arr[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
     public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
-        System.out.println(buyAndSell(prices));
+        int mat[][] = {{1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}};
+        search(mat, 5);
     }
 }
