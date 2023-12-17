66e3c621814c286141fdf326fa9fd280551a6d34
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-17 16:23:49 +0530

    Array Revision

--- Revision/ArrayRevision.java
+++ Revision/ArrayRevision.java
@@ -2,8 +2,8 @@ public class ArrayRevision {
 
     // Linear Search
     public static int linear(int arr[], int key) {
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i]==key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
                 return i;
             }
         }
@@ -13,13 +13,13 @@ public class ArrayRevision {
     // Binary Search
     public static int binary(int arr[], int key) {
         int start = 0;
-        int end = arr.length-1;
-        for(int i=0; i<arr.length; i++) {
-            int mid = (start + end)/2;
-            if(arr[mid] == key) {
+        int end = arr.length - 1;
+        for (int i = 0; i < arr.length; i++) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == key) {
                 return mid;
-            } else if(arr[mid]>key) {
-                end = mid-1;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
@@ -31,7 +31,7 @@ public class ArrayRevision {
     public static void reverse(int arr[]) {
         int start = 0;
         int end = arr.length - 1;
-        while(start<end) {
+        while (start < end) {
             int temp = arr[end];
             arr[end] = arr[start];
             arr[start] = temp;
@@ -43,9 +43,9 @@ public class ArrayRevision {
 
     // Pairs in array
     public static void pairs(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                System.out.print("("+arr[i]+","+arr[j]+")");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
             }
             System.out.println();
         }
@@ -54,47 +54,47 @@ public class ArrayRevision {
     // Largest number
     public static int largest(int arr[]) {
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++) {
-            if(largest<arr[i]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (largest < arr[i]) {
                 largest = arr[i];
             }
         }
         return largest;
     }
-    
+
     // Subarray
     public static void subArray(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for(int j=i; j<arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 int end = j;
-                for(int k=start; k<=end; k++) {
-                    System.out.print(arr[k]+" ");
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
                 }
                 System.out.println();
             }
             System.out.println();
         }
     }
-    
+
     // Max Sum of subarray
     public static void maxSum(int arr[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for(int j=i; j<arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 int end = j;
                 currSum = 0;
-                for(int k=start; k<=end; k++) {
+                for (int k = start; k <= end; k++) {
                     currSum += arr[k];
                 }
-                if(maxSum<currSum) {
+                if (maxSum < currSum) {
                     maxSum = currSum;
                 }
             }
         }
-        System.out.println("max sum is "+maxSum);
+        System.out.println("max sum is " + maxSum);
     }
 
     // Prefix method
@@ -103,17 +103,17 @@ public class ArrayRevision {
         int maxSum = Integer.MIN_VALUE;
         int prefix[] = new int[arr.length];
         prefix[0] = arr[0];
-        for(int i=1; i<arr.length; i++) {
-            prefix[i] = prefix[i-1] + arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
         }
 
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for(int j=i; j<arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 int end = j;
                 currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
             }
-            if(maxSum < currSum) {
+            if (maxSum < currSum) {
                 maxSum = currSum;
             }
         }
@@ -123,19 +123,43 @@ public class ArrayRevision {
     public static void kadens(int arr[]) {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             currSum += arr[i];
-            if(currSum<0) {
+            if (currSum < 0) {
                 currSum = 0;
             }
         }
-        if(maxSum < currSum) {
+        if (maxSum < currSum) {
             maxSum = currSum;
         }
-        System.out.println("max sum is: " +maxSum);
+        System.out.println("max sum is: " + maxSum);
     }
+
+    public static int trapped(int height[]) {
+        // calculating left max boundary
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i=1; i<height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        // calculating right max boundary
+        int rightMax[] = new int[height.length];
+        rightMax[height.length-1] = height[height.length-1];
+        for(int i=height.length-2; i>=0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        // calculating trapped water
+        int trappedWater = 0;
+        for(int i=0; i<height.length; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5 };
-        kadens(arr);
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(trapped(height));
     }
 }
