f8fc4bd3292a211b3578761d774faede0e5e46c5
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-23 17:23:30 +0530

    Queue Revision

--- Revision/QueueRevision.java
+++ Revision/QueueRevision.java
@@ -170,6 +170,112 @@ public class QueueRevision {
             return d.getFirst();
         } 
     }
+
+    // Implementing queue using two stack
+    static class Quee {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data) {
+            while(!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while(!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("Queue empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+
+            return s1.peek();
+        }
+    }
+
+    // Implementing stack using two queue
+    static class Sta{
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public static void push(int data) {
+            if(!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+        }
+
+        public static int pop() {
+            if(isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+
+            int top = -1;
+
+            if(!q1.isEmpty()) {
+                while(!q1.isEmpty()) {
+                    top = q1.remove();
+                    if(q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                while(!q2.isEmpty()) {
+                    top = q2.remove();
+                    if(q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            int top = -1;
+            if(!q1.isEmpty()) {
+                while(!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                while(!q2.isEmpty()) {
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
     public static void main(String[] args) {
         // CirculQueue q = new CirculQueue(5);
         // q.add(1);
@@ -217,16 +323,39 @@ public class QueueRevision {
 
         // Implementing queue using deque
 
-        Queuee q = new Queuee();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
+        // Queuee q = new Queuee();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
 
-        System.out.println(q.peek());
-        System.out.println(q.remove());
-        System.out.println(q.remove());
-        System.out.println(q.remove());
-        System.out.println(q.remove());
+        // System.out.println(q.peek());
+        // System.out.println(q.remove());
+        // System.out.println(q.remove());
+        // System.out.println(q.remove());
+        // System.out.println(q.remove());
+
+        // Implementing queue using two stack
+
+        // Quee q = new Quee();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+
+        // while(!q.isEmpty()) {
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+        // Implementing Stack using two queue
+
+        Sta s = new Sta();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
     }
 }
