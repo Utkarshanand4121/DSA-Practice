0100a11460ca43d9a3469f15093a1aec84d15658
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-25 20:20:57 +0530

    Revision

--- Revision/BTRevision.java
+++ Revision/BTRevision.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+
+import org.w3c.dom.Node;
 
 public class BTRevision {
 
@@ -146,7 +149,7 @@ public class BTRevision {
     }
 
     public static Info diameter2(Node root) {
-        if(root == null) {
+        if (root == null) {
             return new Info(0, 0);
         }
 
@@ -155,22 +158,181 @@ public class BTRevision {
 
         int diam = Math.max(Math.max(leftInfo.dia, rightInfo.dia), leftInfo.ht + rightInfo.ht + 1);
         int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
-        
+
         return new Info(diam, ht);
     }
 
+    // Subtree of a tree
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+        if (!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+
+        return isIdentical(root.left, subRoot) || isIdentical(root.right, subRoot);
+    }
+
+    // Top view
+    static class Info2 {
+        Node node;
+        int hd;
+
+        public Info2(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node root) {
+        Queue<Info2> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min = 0, max = 0;
+
+        q.add(new Info2(root, 0));
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Info2 curr = q.remove();
+            if (curr == null) {
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+                }
+
+                if (curr.node.left != null) {
+                    q.add(new Info2(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new Info2(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
+    // Kth level
+    public static void kLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        kLevel(root.left, level + 1, k);
+        kLevel(root.right, level + 1, k);
+    }
+
+    // Kth ancestor of node
+    public static int kthAncestor(Node root, int n, int k) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = kthAncestor(root.left, n, k);
+        int rightDist = kthAncestor(root.right, n, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist);
+
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+        return max + 1;
+    }
+
+    // Transform to sum Tree
+    public static int sumTree(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftChild = sumTree(root.left);
+        int rightChild = sumTree(root.right);
+
+        int data = root.data;
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = leftChild + rightChild + newLeft + newRight;
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.right = new Node(4);
+        // Node root = new Node(1);
+        // root.left = new Node(2);
+        // root.right = new Node(3);
+        // root.left.right = new Node(4);
 
         // System.out.println(height(root));
         // System.out.println(count(root));
         // System.out.println(sum(root));
-        System.out.println(diameter(root));
+        // System.out.println(diameter(root));
 
         // levelOrder(root);
-        System.out.println(diameter2(root).dia);
+        // System.out.println(diameter2(root).dia);
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // Node subRoot = new Node(2);
+        // subRoot.left = new Node(4);
+        // subRoot.right = new Node(5);
+
+        // System.out.println(isSubtree(root, subRoot));
+        // topView(root);
+
+        // kLevel(root, 1, 2);
+        System.out.println(kthAncestor(root, 5, 1));
+        sumTree(root);
+        preorder(root);
     }
 }
