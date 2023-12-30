0d7ae5b67a699650f1e72987823ee7154ab500fc
Utkarsh Anand <utkarshanand4121@gmail.com>
2023-12-30 13:08:59 +0530

    Good

--- DSA Practice/Array.java
+++ DSA Practice/Array.java
@@ -963,6 +963,50 @@ public class Array {
         return count;
     }
 
+    public static int missingNumber(int A[], int N)
+    {
+        // Your code goes here
+        int sum1 = (N*(N+1))/2;
+        int sum2 = 0;
+        for(int i = 0; i<A.length; i++) {
+            sum2 += A[i];
+        }
+        int res = sum1 - sum2;
+        return res;
+    }
+
+    public boolean possible(long arr[], long n)
+    {
+        long sum = (n*(n+1))/2;
+        long sum1 = 0;
+        for(int i=0; i<arr.length; i++) {
+            sum1 += arr[i];
+        }
+        return sum == sum1;
+    }
+
+    static String winner(int x, int m, int n, long arr[])
+    {
+        // code here
+        String res = "";
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] % m == 0) {
+                count1++;
+            } else if(arr[i] % n == 0) {
+                count2++;
+            } 
+        }
+        if(count1 > count2) {
+            res = "Ram";
+        } else if(count1 == count2) {
+            res = "Both";
+        } else {
+            res = "Rohan";
+        }
+        return res;
+    }
     public static void main(String[] args) {
 
     }
