8d62407436b70b23c13ca0263cd5a47f3a8ff371
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-02-25 10:56:30 +0530

    Revision

--- Revision/BitRevision.java
+++ Revision/BitRevision.java
@@ -65,8 +65,53 @@ public class BitRevision {
         return ans;
     }
 
+    public static void evenOdd2(int n) {
+        int bitMask = 1;
+        if((n & bitMask) == 1) {
+            System.out.println("odd number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+
+    public static int getIthBit2(int n, int i) {
+        int bitMask = 1<<i;
+        if((n & bitMask) == 0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public static int setIthBit(int n, int key) {
+        int bitMask = 1<<key;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int key) {
+        int bitMask = ~(1 << key);
+        return n & bitMask;
+    }
+
+    public static int updateBit2(int n, int key, int newBit) {
+        if(newBit == 0) {
+            return clearIthBit(n, key);
+        } else {
+            return setIthBit(n, key);
+        }
+    }
+
+    public static int clearRange2(int n, int key) {
+        int bitMask = (~0) << key;
+        return n & bitMask;
+    }
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(fastExpo(n, 3));
+        // int n = 5;
+        // System.out.println(fastExpo(n, 3));
+        evenOdd2(5);
+        System.out.println(getIthBit2(15, 3));
+        System.out.println(setIthBit(15, 4));
+        System.out.println(clearIthBit(16, 4));
+        System.out.println(updateBit2(16, 4, 0));
+        System.out.println(clearRange2(15, 3));
     }
 }
