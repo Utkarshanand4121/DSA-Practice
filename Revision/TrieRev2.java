2e088577f9b8e060d160daec3637053e663512fa
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-05-22 15:46:34 +0530

    Nice

---
+++ Revision/TrieRev2.java
@@ -0,0 +1,62 @@
+public class TrieRev2 {
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for(int i=0; i<children.length; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) { // O(L)
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String word) { // O(L)
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static boolean wordBreak(String key) { // O(L) --> L = length of key
+        if(key.length() == 0) {
+            return true;
+        }
+        for(int i=1; i<=key.length(); i++) {
+            if(search(key.substring(0, i)) && wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String word[] = {"the", "a", "there", "their", "any", "thee"};
+        for(int i=0; i<word.length; i++) {
+            insert(word[i]);
+        }
+        System.out.println(search("thee"));
+        System.err.println(search("thor"));
+
+        System.out.println(wordBreak("thereatheir"));
+    }
+}
