cd9e0bb84e726cdf083fcf7ece17e9acb8710dec
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-05-28 17:09:51 +0530

    Revision

--- Revision/Graph.java
+++ Revision/Graph.java
@@ -355,6 +355,57 @@ public class Graph {
             printallpath(graph, e.dest, dest, path + src);
         }
     }
+
+    // Dijkstra's Algorithm
+    static class Pair implements Comparable<Pair> {
+        int n;
+        int path;
+
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+    public static void dijkstra(ArrayList<Edge> graph[], int src) {
+        int dis[] = new int[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(i != src) {
+                dis[i] = Integer.MAX_VALUE;
+            }
+        }
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if(!vis[curr.n]) {
+                vis[curr.n] = true;
+                for(int i=0; i<graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if(dis[u] + wt < dis[v]) { // update distance of src to v
+                        dis[v] = dis[u] + wt;
+                        pq.add(new Pair(v, dis[v]));
+                    }
+                }
+            }
+        }
+
+        // print all path from src to dest
+        for(int i=0; i<dis.length; i++) {
+            System.out.print(dis[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
@@ -371,6 +422,7 @@ public class Graph {
 
         // System.out.println(isCycle(graph));
 
-        topSort(graph);
+        // topSort(graph);
+        dijkstra(graph, 0);
     }
 }
