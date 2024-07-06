3988e6d70fe1f320b339c00feff93d87d657f73b
Utkarsh Anand <utkarshanand4121@gmail.com>
2024-07-06 11:07:45 +0530

    BT Revision

--- Revision/BT2Revision.java
+++ Revision/BT2Revision.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -202,7 +203,7 @@ public class BT2Revision {
 
             q.add(new Info2(root, 0));
             q.add(null);
-            
+
             int min = 0;
             int max = 0;
 
@@ -235,7 +236,109 @@ public class BT2Revision {
             }
             System.out.println();
         }
+
+        public static void klevel(Node root, int level, int k) {
+            if (root == null) {
+                return;
+            }
+
+            if (level == k) {
+                System.out.print(root.data + " ");
+                return;
+            }
+            klevel(root.left, level + 1, k);
+            klevel(root.right, level + 1, k);
+        }
+
+        public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+
+            if(root == null) {
+                return false;
+            }
+
+            path.add(root);
+
+            if(root.data == n) {
+                return true;
+            }
+            boolean foundLeft = getPath(root.left, n, path);
+            boolean foundRight = getPath(root.right, n, path);
+
+            if(foundLeft || foundRight) {
+                return true;
+            }
+
+            path.remove(path.size() - 1);
+            return false;
+        }
+
+        public static Node lca(Node root, int n1, int n2) {
+            ArrayList<Node> path1 = new ArrayList<>();
+            ArrayList<Node> path2 = new ArrayList<>();
+
+            getPath(root, n1, path1);
+            getPath(root, n2, path2);
+
+            // last common ancestor
+            int i = 0;
+            for(; i<path1.size() && i<path2.size(); i++) {
+                if(path1.get(i) != path2.get(i)) {
+                    break;
+                }
+            }
+
+            Node lca = path1.get(i-1);
+            return lca;
+        }
+
+        public static Node lca2(Node root, int n1, int n2) {
+            if(root == null || root.data == n1 || root.data == n2) {
+                return root;
+            }
+
+            Node leftLca = lca2(root.left, n1, n2);
+            Node rightLca = lca2(root.right, n1, n2);
+
+            if(rightLca == null) {
+                return leftLca;
+            }
+
+            if(leftLca == null) {
+                return rightLca;
+            }
+
+            return root;
+        }
+
+        // Min Dist
+        public static int lcaDist(Node root, int n) {
+            if(root == null) {
+                return -1;
+            }
+
+            if(root.data == n) {
+                return 0;
+            }
+            int leftDist = lcaDist(root.left, n);
+            int rightDist = lcaDist(root.right, n);
+
+            if(leftDist == -1 && rightDist == -1) {
+                return -1;
+            } else if(leftDist == -1) {
+                return rightDist + 1;
+            } else {
+                return leftDist + 1;
+            }
+        }
+        public static int minDist(Node root, int n1, int n2) {
+            Node lca = lca2(root, n1, n2);
+            int dist1 = lcaDist(root, n1);
+            int dist2 = lcaDist(root, n2);
+
+            return dist1 + dist2;
+        }
     }
+
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
@@ -248,9 +351,17 @@ public class BT2Revision {
         // System.out.println(tree.height(root));
         // System.out.println(tree.cnt(root));
         // System.out.println(tree.sum(root));
-        System.out.println(tree.diameter(root));
+        // System.out.println(tree.diameter(root));
 
-        System.out.println(tree.dia2(root).dia);
-        tree.topView(root);
+        // System.out.println(tree.dia2(root).dia);
+        // tree.topView(root);
+
+        // tree.klevel(root, 1, 3);
+        // System.out.println(tree.lca(root, 4, 6).data);
+
+        // System.out.println(tree.lca2(root, 4, 5).data);
+
+        System.out.println(tree.minDist(root, 4, 6));
+        
     }
 }
